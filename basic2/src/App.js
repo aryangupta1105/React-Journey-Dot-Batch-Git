e438788772287ab7f296edc9efe248ea0d3e6513
aryangupta1105 <husltegamingfanclub@gmail.com>
2024-03-22 23:59:26 +0530

    updated components

--- basic2/src/App.js
+++ basic2/src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import Product from "./components/Product"
 
 function App() {
+  const products = 
+    [
+      {
+        id: "01",
+        title: "Nirma", 
+        price: "100",
+        data: new Date(2024 , 8 , 10),
+      },
+      {
+        id: "02",
+        title: "Maggie", 
+        price: "50",
+        data: new Date(2024 , 4 , 11),
+      },
+      {
+        id: "03",
+        title: "Surf-Excel", 
+        price: "150",
+        data: new Date(2024 , 8 , 22),
+      },
+      {
+        id: "04",
+        title: "Vimal", 
+        price: "40",
+        data: new Date(2024 , 2 , 10),
+      },
+    ]
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Product items={products}></Product>
     </div>
   );
 }
