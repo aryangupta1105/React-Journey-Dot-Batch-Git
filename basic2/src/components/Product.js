18514487791e126224d97f059038d90e1ca36912
aryangupta1105 <husltegamingfanclub@gmail.com>
2024-03-22 23:57:40 +0530

    Product.js

---
+++ basic2/src/components/Product.js
@@ -0,0 +1,33 @@
+import "./Product.css"
+import ProductItem from "./ProductItem.js"
+
+
+function Product (props) {
+    return (
+        <Card className="Products">
+            <ProductItem>
+                title = {props.item[0].name}
+                amount = {props.item[0].price}
+                date = {props.item[0].date}
+            </ProductItem>
+            <ProductItem>
+                title = {props.item[1].name}
+                amount = {props.item[1].price}
+                date = {props.item[1].date}
+            </ProductItem>
+            <ProductItem>
+                title = {props.item[2].name}
+                amount = {props.item[2].price}
+                date = {props.item[2].date}
+            </ProductItem>
+            <ProductItem>
+                title = {props.item[3].name}
+                amount = {props.item[3].price}
+                date = {props.item[3].date}
+            </ProductItem>
+        </Card>
+        
+    );
+}
+
+export default Product;
